2dbaabedac80639d4878ef4dbc4cfca80d10957a
विनय कार्की <46816702+Binay7587@users.noreply.github.com>
2022-08-09 07:45:13 +0545

    Rendering the component for tests

---
+++ frontend/src/components/Note.test.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import '@testing-library/jest-dom/extend-expect'
+import { render, screen } from '@testing-library/react'
+import Note from './Note'
+
+test('renders content', () => {
+  const note = {
+    content: 'Component testing is done with react-testing-library',
+    important: true
+  }
+
+  render(<Note note={note} />)
+
+  const element = screen.getByText('Component testing is done with react-testing-library')
+  expect(element).toBeDefined()
+})
